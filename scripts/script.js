e731537aa25c4edfb763fa5d5d7c69312f2533e9
TheButlerDidIt <labutler@gmail.com>
2016-12-05 17:45:52 -0500

    outline assignment

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// A single page app allowing the user to play a simple game of Wheel of Fortune. 
+// Use JavaScript to give the user a word to guess letter by letter. 
+// optional: Will the user lose points as they guess wrong?  Will they try to beat the clock? Do they only get 3 guesses? 
+// You decide! Make it clear to the user what their goal is. 
+// The app should be styled with simple but modern design trends and fun for the user. 
+// Remember keep your user engaged.
+//several rounds of new words
+// Hints are given to direct the user what the word is related to
+// extra challenge: spinning wheel & money
+
+
+//give user a word to guess, letter by letter
+//Make it clear to the user what their goal is. (ie # guesses, buying vowels)
